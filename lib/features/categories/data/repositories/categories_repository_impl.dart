2190f50b5f8abe8cee4ef23010bff9858ba707ab
Mamus <eferhamamus@gmail.com>
2021-05-18 23:35:03 +0100

    feat:implement categories repository class

---
+++ lib/features/categories/data/repositories/categories_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_colony/core/error/exception.dart';
+import 'package:movie_colony/core/error/failure.dart';
+import 'package:movie_colony/core/network/network_info.dart';
+import 'package:movie_colony/features/categories/data/datasources/categories_local_data_source.dart';
+import 'package:movie_colony/features/categories/data/datasources/categories_remote_data_source.dart';
+import 'package:movie_colony/features/categories/domain/entities/categories.dart';
+import 'package:movie_colony/features/categories/domain/repositories/categories_repository.dart';
+
+class CategoriesRepositoryImpl implements CategoriesRepository {
+  final CategoriesRemoteDataSource remoteDataSource;
+  final CategoriesLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  CategoriesRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+  @override
+  Future<Either<Failure, List<Categories>>> getCategories() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteCategory = await remoteDataSource.getRemoteCategories();
+        localDataSource.cacheLastCategory(remoteCategory);
+        return Right(remoteCategory);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localCategory = await localDataSource.getCachedCategory();
+        return Right(localCategory);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
+  }
+}
