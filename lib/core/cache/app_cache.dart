c13bca387d2d34528ab5a34a925f6033ece345a3
Mamus <eferhamamus@gmail.com>
2021-05-25 11:03:29 +0100

    feat:app cache and unit test

---
+++ lib/core/cache/app_cache.dart
@@ -0,0 +1,27 @@
+import 'package:movie_colony/core/custom_shared_preference/custom_shared_preference.dart';
+import 'package:movie_colony/core/utils/strings.dart';
+
+abstract class AppCache {
+  final CustomSharedPreference pref;
+
+  AppCache(this.pref);
+
+  ///check if the date is expired by comparing with current date. If current date is after expiry date return true, else return false
+  bool isExpired(String key);
+}
+
+class AppCacheImpl implements AppCache {
+  final CustomSharedPreference pref;
+
+  AppCacheImpl(this.pref);
+  @override
+  bool isExpired(String key) {
+    String? value = pref.retrieveData(key + EXPIRY_DATE);
+    if (value != null) {
+      var currentDate = DateTime.now();
+      return currentDate.isAfter(DateTime.parse(value));
+    } else {
+      return true;
+    }
+  }
+}
